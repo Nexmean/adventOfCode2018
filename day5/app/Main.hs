8f3e09e07a096266f43f5295dbe914c38531d38e
Alexander Makarov <next.meaning@gmail.com>
2018-12-08 10:50:49 +1000

    Solve second part of 5th day

--- day5/app/Main.hs
+++ day5/app/Main.hs
@@ -3,21 +3,31 @@ module Main where
 
 import           Control.Monad.State
 import           Data.Char
+import           Data.Foldable
 import           System.Environment
 
 main :: IO ()
 main = do
   (filename : _) <- getArgs
   s              <- readFile filename
-  print $ length (solve1 s)
+  print $ solve1 s
+  print $ solve2 s
 
-solve1 :: String -> String
-solve1 s = if length s == length (reduce s) then s else solve1 $ reduce s
+solve1 :: String -> Int
+solve1 = length . fullyReduce
+
+solve2 :: String -> Int
+solve2 s = minimum
+  $ map (length . fullyReduce)
+  $ map (\c -> filter (\c' -> c' /= toLower c && c' /= toUpper c) s) ['a' .. 'z']
+
+fullyReduce :: String -> String
+fullyReduce s = if length s == length (reduce s) then s else fullyReduce $ reduce s
 
 reduce = foldr helper ""
  where
   helper c [] = [c]
-  helper c r  = if reducible c (head r) then tail r else c : r
+  helper c s  = if reducible c (head s) then tail s else c : s
 
   reducible a b =
     ((isLower a && isUpper b) || (isUpper a && isLower b))
