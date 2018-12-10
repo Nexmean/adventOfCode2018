3fffb9c967d45230e1652358a2816390230ee40a
Alexander Makarov <next.meaning@gmail.com>
2018-12-10 20:59:12 +1000

    Solved second part of 9th day

--- day9/app/Main.hs
+++ day9/app/Main.hs
@@ -1,10 +1,15 @@
 module Main where
 
+import           Control.Applicative
 import           Control.Monad.State
+import           Data.Maybe
 import           Data.List
-import qualified Data.Map.Strict                      as Map
+import qualified Data.List.PointedList         as PL
+import qualified Data.Map.Lazy                 as Map
 import           System.Environment
--- import           Text.Parsec
+import           Text.Parsec             hiding ( (<|>)
+                                                , many
+                                                )
 
 main :: IO ()
 main = do
@@ -12,52 +17,53 @@ main = do
   print $ solve1 (read players) (read marble)
 
 solve1 :: Int -> Int -> Int
-solve1 players marble = maximum $ Map.elems $ play players marble
+solve1 players marble = maximum $ Map.elems $ play players (Marble marble)
 
 type Player = Int
-type Marble = Int
+newtype Marble = Marble { getMarble :: Int } deriving (Eq, Show)
 
-data GameState = GS { _marbles       :: [Marble]
-                    , _currentMarble :: Marble
-                    , _points        :: Map.Map Player Int
+type Circle = PL.PointedList Marble
+
+data GameState = GS { _circle :: Circle
+                    , _points :: Map.Map Player Int
                     }
-
-insertIn :: a -> Int -> [a] -> [a]
-insertIn el 0 xs       = el : xs
-insertIn el i (x : xs) = x : insertIn el (i - 1) xs
 
 play :: Int -> Marble -> Map.Map Player Int
 play playersCount lastMarble =
-  let turns = zip (concat . repeat $ [0 .. playersCount - 1]) [0 .. lastMarble]
+  let turns = zip (concat . repeat $ [0 .. playersCount - 1])
+                  (map Marble [0 .. getMarble lastMarble])
   in  _points
-      $ (`execState` GS [] undefined Map.empty)
+      $ (`execState` GS undefined Map.empty)
       $ forM turns
       $ \(player, marble) -> do
-          ms <- gets _marbles
-          cm <- gets _currentMarble
-          if marble == 0 || marble `mod` 23 /= 0
+          circle <- gets _circle
+          if getMarble marble == 0 || getMarble marble `mod` 23 /= 0
             then do
-              let (newCm, newMs) = commonTurn ms cm marble
-              put =<< GS newMs newCm <$> gets _points
+              let newCircle = commonTurn circle marble
+              put =<< GS newCircle <$> gets _points
             else do
-              let (newCm, newMs, addPoints) = specificTurn ms cm marble
+              let (newCircle, addPoints) = specificTurn circle marble
                   alterHelper (Just points) = Just $ points + addPoints
                   alterHelper Nothing       = Just addPoints
               newPoints <- Map.alter alterHelper player <$> gets _points
-              put $ GS newMs newCm newPoints
+              put $ GS newCircle newPoints
 
-commonTurn :: [Marble] -> Marble -> Marble -> (Marble, [Marble])
-commonTurn ms cm m =
-  let cmIdx = case cm `elemIndex` ms of
-        Just idx -> idx
-      placeIndex = (cmIdx + 2) `mod` length ms
-  in  if m /= 0 then (m, insertIn m placeIndex ms) else (0, [0])
+commonTurn :: Circle -> Marble -> Circle
+commonTurn circle marble = if marble == Marble 0
+  then PL.singleton marble
+  else
+    fromMaybe undefined
+    $   PL.insert marble
+    <$> (PL.moveN 1 circle <|> scrolledCircle)
+ where
+  scrolledCircle =
+    PL.moveTo ((PL.index circle + 1) `mod` PL.length circle) circle
 
-specificTurn :: [Marble] -> Marble -> Marble -> (Marble, [Marble], Int)
-specificTurn ms cm m =
-  let cmIdx = case cm `elemIndex` ms of
-        Just idx -> idx
-      rmIdx    = if cmIdx - 7 >= 0 then cmIdx - 7 else length ms + (cmIdx - 7)
-      newCmIdx = if rmIdx >= length ms - 1 then 0 else rmIdx
-      newMs    = delete (ms !! rmIdx) ms
-  in  (newMs !! newCmIdx, newMs, m + ms !! rmIdx)
+specificTurn :: Circle -> Marble -> (Circle, Int)
+specificTurn circle (Marble points) = (reduced, points + addPoint)
+ where
+  reduced = fromMaybe undefined $ PL.delete moved
+  moved =
+    fromMaybe undefined $ PL.moveN (-7) circle <|> PL.moveTo scrollN circle
+  scrollN           = PL.length circle + (PL.index circle - 7)
+  (Marble addPoint) = PL._focus moved
